ea02bdc4f943891ad70e212795724fc9bf2a32fe
VincevanderBerg <vincevanderberg@gmail.com>
2023-12-17 17:30:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tempest_weather_app
+
+A weather app written in Flutter that pulls live weather data from the public OpenMeteo API.
---
+++ android/app/src/main/java/com/codemountain/tempest_weather_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.codemountain.tempest_weather_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
